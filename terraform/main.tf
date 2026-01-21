1f5078457c82f00925efa1a5f6d835bff4f0267e
Rohith M <rohith9m@gmail.com>
2026-01-21 18:51:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,39 @@ resource "aws_iam_role" "lambda_role" {
     }]
   })
 }
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.project_name}-${var.environment}-lambda-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = aws_sqs_queue.log_queue.arn
+      },
+      {
+        Effect = "Allow"
+        Action = "sns:Publish"
+        Resource = aws_sns_topic.alerts_topic.arn
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
