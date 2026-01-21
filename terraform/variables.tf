b3f7ccdd59239ca1910573e2371907712e3792f5
Rohith M <rohith9m@gmail.com>
2026-01-21 17:25:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,12 @@
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "centralized-log-monitoring"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
 
