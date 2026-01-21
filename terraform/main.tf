a5e76a2149a56a3cf8a3a969f6c3c885578ff61d
Rohith M <rohith9m@gmail.com>
2026-01-21 17:40:11 +0530

    Add IAM role and policy for Lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,4 +9,17 @@ resource "aws_sqs_queue" "log_queue" {
 resource "aws_sns_topic" "alerts_topic" {
   name = "${var.project_name}-${var.environment}-alerts"
 }
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.environment}-lambda-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
