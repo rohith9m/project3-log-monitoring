79d1cd2ab840fa5545789d23545d143e3c9e00ec
Rohith M <rohith9m@gmail.com>
2026-01-21 17:26:29 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
 
+output "s3_bucket_name" {
+  value = aws_s3_bucket.log_bucket.bucket
+}
+
+output "sqs_queue_name" {
+  value = aws_sqs_queue.log_queue.name
+}
+
+output "sns_topic_name" {
+  value = aws_sns_topic.alerts_topic.name
+}
