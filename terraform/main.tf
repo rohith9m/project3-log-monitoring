57f73ec0761fc31a5d050b00b9d00669f0ba7ecd
Rohith M <rohith9m@gmail.com>
2026-01-21 17:26:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "${var.project_name}-${var.environment}-logs"
+}
+
+resource "aws_sqs_queue" "log_queue" {
+  name = "${var.project_name}-${var.environment}-queue"
+}
+
+resource "aws_sns_topic" "alerts_topic" {
+  name = "${var.project_name}-${var.environment}-alerts"
+}
 
